d668e033dc042060674b99be95c5ca1176dcc933
Eriny Adel <e.adel@singleclic.com>
2025-10-26 14:22:28 +0300

    Test CICD Pipeline

--- BloggingPlatform.Application/CommandsAndQueries/Commands/Posts/CreatePostCommand.cs
+++ BloggingPlatform.Application/CommandsAndQueries/Commands/Posts/CreatePostCommand.cs
@@ -1,11 +1,4 @@
 ﻿using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.Text;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 
 namespace BloggingPlatform.Application.CommandsAndQueries.Commands.Posts
 {
